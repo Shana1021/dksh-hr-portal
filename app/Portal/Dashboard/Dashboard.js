ba4ecc869aeae631c44523da6800cb270945ffd8
rasul <rasulp99@.com>
2023-08-12 19:18:23 +0800

    changes

--- app/Portal/Dashboard/Dashboard.js
+++ app/Portal/Dashboard/Dashboard.js
@@ -1,159 +1,25 @@
 "use client";
+import { useSession } from "next-auth/react";
 
-import "./dashboard.css";
-import Image from "next/image";
-import "boxicons";
+export default function Home({ data }) {
+  const { data: session, status } = useSession();
 
-export default function Dashboard() {
+  console.log(session);
+  console.log(status);
+
   return (
     <>
-      <div className="sidebar close">
-        <div className="logo-details">
-          <Image
-            src="/dksh_logo.png"
-            alt="DKSH Logo"
-            fill
-            // sizes="50px"
-            style={{ objectFit: "contain" }}
-            placeholder="blur"
-            blurDataURL="/dksh_logo.png"
-          />
-          <span className="logo_name">HR Portal</span>
-        </div>
-        <ul className="nav-links">
-          <li>
-            <a href="#">
-              <i className="bx bxs-dashboard"></i>
-              <span className="link_name">Dashboard</span>
-            </a>
-            <ul className="sub-menu blank">
-              <li>
-                <a className="link_name" href="#">
-                  Dashboard
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <div className="iocn-link">
-              <a href="#">
-                <box-icon type="solid" name="pear"></box-icon>
-                <span className="link_name">Onboarding</span>
-              </a>
-              <i className="bx bxs-chevron-down arrow"></i>
-            </div>
-            <ul className="sub-menu">
-              <li>
-                <a className="link_name" href="#">
-                  Onboarding
-                </a>
-              </li>
-              <li>
-                <a href="#">Background Check</a>
-              </li>
-              <li>
-                <a href="#">CheckList</a>
-              </li>
-              <li>
-                <a href="#">Probationary</a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <div className="iocn-link">
-              <a href="#">
-                <i className="bx bx-book"></i>
-                <span className="link_name">Training</span>
-              </a>
-              <i className="bx bxs-chevron-down arrow"></i>
-            </div>
-            <ul className="sub-menu">
-              <li>
-                <a className="link_name" href="#">
-                  Training
-                </a>
-              </li>
-              <li>
-                <a href="#">Request Training</a>
-              </li>
-              <li>
-                <a href="#">Status Check</a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <div className="iocn-link">
-              <a href="#">
-                <i className="bx bx-notepad"></i>
-                <span className="link_name">Offboarding</span>
-              </a>
-              <i className="bx bxs-chevron-down arrow"></i>
-            </div>
-            <ul className="sub-menu">
-              <li>
-                <a className="link_name" href="#">
-                  Offboarding
-                </a>
-              </li>
-              <li>
-                <a href="#">Request for Resignation</a>
-              </li>
-              <li>
-                <a href="#">Resignation Requests</a>
-              </li>
-              <li>
-                <a href="#">CheckList</a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a href="#">
-              <i className="bi bi-123"></i>
-              <span className="link_name">HR List</span>
-            </a>
-            <ul className="sub-menu blank">
-              <li>
-                <a className="link_name" href="#">
-                  HR List
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a href="#">
-              <i className="bx bx-cog"></i>
-              <span className="link_name">Settings</span>
-            </a>
-            <ul className="sub-menu blank">
-              <li>
-                <a className="link_name" href="#">
-                  Settings
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <div className="profile-details">
-              <div className="profile-content">
-                <Image
-                  src="/dksh_logo.png"
-                  alt="DKSH Logo"
-                  fill
-                  // sizes="50px"
-                  style={{ objectFit: "contain" }}
-                  placeholder="blur"
-                  blurDataURL="/dksh_logo.png"
-                />
-              </div>
-              <div className="name-job">
-                <div className="profile_name">Jared</div>
-                <div className="job">HR Manager</div>
-              </div>
-              <i className="bx bx-log-out"></i>
-            </div>
-          </li>
-        </ul>
-      </div>
+      <h1>Dashboard</h1>
+      <p>Fetched data:</p>
+      <ul>
+        {data &&
+          data.map((d) => (
+            <li key={d.id}>
+              {d.test}
+              {d._id}
+            </li>
+          ))}
+      </ul>
     </>
   );
 }
--- app/Portal/Dashboard/page.js
+++ app/Portal/Dashboard/page.js
@@ -1,9 +1,10 @@
 import Dashboard from "./Dashboard.js";
-
+import Sidebar from "./Sidebar.js";
 export default async function DashboardPage() {
-  
-
   return (
-    <Dashboard />
-  )
+    <>
+      <Dashboard />
+      <h1>Hello World</h1>
+    </>
+  );
 }
