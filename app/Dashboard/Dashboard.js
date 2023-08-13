01a3f817c21dcce1580c1b170cf457f6372b8e0d
rasul <rasulp99@.com>
2023-08-13 09:57:19 +0800

    new

--- app/Dashboard/Dashboard.js
+++ app/Dashboard/Dashboard.js
@@ -1,13 +1,13 @@
 "use client";
 import { signIn, signOut, useSession } from "next-auth/react";
 
-export default function Home() {
+export default function Home({ data: name, _id }) {
   const { data: session, status } = useSession();
 
   if (status === "loading") {
     return <>Loading...</>;
   }
-  
+
   if (status === "unauthenticated") {
     signIn();
     return <></>;
@@ -18,7 +18,12 @@ export default function Home() {
       <h1>Dashboard</h1>
       ID: {session.user._id}
       <br />
-      <button onClick={() => signOut({callbackUrl: "/Login"})}>Test sign out button</button>
+      {data.map((d) => (
+        <h1>{d.id}</h1>
+      ))}
+      <button onClick={() => signOut({ callbackUrl: "/Login" })}>
+        Test sign out button
+      </button>
     </>
   );
 }
