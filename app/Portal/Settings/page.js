84dc728ad6492750559924a6015b634ae0158fef
rasul <rasulp99@.com>
2023-08-22 09:38:38 +0800

    show password feature in login

--- app/Portal/Settings/page.js
+++ app/Portal/Settings/page.js
@@ -27,15 +27,30 @@ export default function Settings() {
   };
   return (
     <>
-      <form ref={form} onSubmit={sendEmail}>
-        <label className="label">Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
-      </form>
+      <h2>System Email Settings</h2>
+      <div className="Container">
+        <form ref={form} onSubmit={sendEmail}>
+          <label className="label">Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
+      </div>
+      <h2>Recepient Email Settings</h2>
+      <div className="Container">
+        <form ref={form} onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
+      </div>
     </>
   );
 }
