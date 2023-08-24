04a96eb86e5c79aa4b630281848746b1425f9c4b
DesmondDui0526 <desmonddui0526@gmail.com>
2023-08-24 20:37:55 +0800

    I've added input email column, upload image, header and also change some css style.

--- app/Portal/NewEmployee/page.js
+++ app/Portal/NewEmployee/page.js
@@ -4,59 +4,88 @@ import "./style.css";
 
 export default function NewEmployee() {
   const [passwordVisible, setPasswordVisible] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   const togglePasswordVisibility = () => {
     setPasswordVisible((prevState) => !prevState);
   };
 
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setSelectedImage(URL.createObjectURL(file));
+    }
+  };
   return (
     <>
-      <h1>New Employee</h1>
+      <h1>HR PROFILE</h1>
       <div className="Container">
         <input type="text" placeholder="Enter First Name" required />
+        <input type="email" placeholder="Enter Employee Email" required />
         <input type="text" placeholder="Enter Middle Name" required />
-        <input type="text" placeholder="Enter Last Name" required />
         <input
           type={passwordVisible ? "text" : "password"}
           placeholder="Enter Employee Password"
           required
         />
-        <input type="text" placeholder="Enter Position" required />
-        <input type="text" placeholder="Enter Department" required />
-        <input type="text" placeholder="Enter Employee ID" required />
-        <input type="text" placeholder="Enter Address Line 1" required />
-        <input type="text" placeholder="Enter Address Line 2" required />
-        <input type="text" placeholder="Enter City" required />
-        <input type="text" placeholder="Enter State" required />
-        <input type="text" placeholder="Enter Postal Code" required />
-
-        <input
-          type="tel"
-          placeholder="Enter Phone Number"
-          pattern="[0-9]{2}-[0-9]{3}-[0-9]{4}"
-          required
-        />
-        <input type="date" />
-        <input
-          type={passwordVisible ? "text" : "password"}
-          placeholder="Enter Password"
-          required
-          id="password"
-        />
+        <input type="text" placeholder="Enter Last Name" required />
         <label>
-          <input type="checkbox" onClick={togglePasswordVisibility} />
           Show Password
+          <input type="checkbox" onClick={togglePasswordVisibility} />
         </label>
-      </div>
-      <div className="radio">
-        <label htmlFor="male"> Male</label>
-        <input type="radio" id="male" name="malefemale" value="male" />
 
-        <label htmlFor="female">Female</label>
-        <input type="radio" id="female" name="malefemale" value="female" />
+        <input type="text" placeholder="Enter Employee ID" required />
+
+        
+        <div className="radio">
+          <label htmlFor="male" className="bigger-label">Gender </label><br/>
+          <label htmlFor="male" className="bigger-label"> Male</label>
+          <input type="radio" id="male" name="malefemale" value="male" />
+          <label htmlFor="female" className="bigger-label">Female</label>
+          <input type="radio" id="female" name="malefemale" value="female" />
+        </div>
+
+        <input type="text" placeholder="Enter Address Line 1" required />
+
+        <input type="date" required/>
+        
+        <input type="text" placeholder="Enter Address Line 2" required />
+        <div className="city-state-container">
+          <input type="text" placeholder="Enter City" required />
+          <input type="text" placeholder="Enter State" required />
+        </div>
+        
+        <div className="image-upload">
+          <label htmlFor="image">Upload Image:</label>
+          <input
+            type="file"
+            id="image"
+            accept="image/*" 
+            onChange={handleImageChange}
+          />
+          {selectedImage && (
+            <img
+              src={selectedImage}
+              alt="Selected Employee Image"
+              className="uploaded-image"
+            />
+          )}
+        </div>
+
+        <div className="postal-phone-container">
+          <input type="text" placeholder="Enter Postal Code" required />
+          <input
+            type="tel"
+            placeholder="Enter Phone Number"
+            pattern="[0-9]{2}-[0-9]{3}-[0-9]{4}"
+            required
+          />
+        </div>
+
       </div>
+
       <div className="Button">
-        <input type="submit" />
+        <input type="submit" value="Save" className="bigger-button"/>
       </div>
     </>
   );
