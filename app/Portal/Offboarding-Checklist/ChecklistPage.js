e101d774b1a9ae03e8a12727e8745d852378727e
Shana1021 <nakshatramaalah@gmail.com>
2023-08-29 14:27:51 +0800

    Offboarding checklist - first draft

---
+++ app/Portal/Offboarding-Checklist/ChecklistPage.js
@@ -0,0 +1,35 @@
+import styles from "./ofc.module.css";
+import Table from "../Table";
+import Link from "next/link";
+
+export default function ChecklistPage({ checklistItems }) {
+  for (const item of checklistItems) {
+    item.checklist = (
+      <>
+        <button className={styles["checklist-button"]}>Checklist</button>
+      </>
+    );
+  }
+  return (
+    <div className={styles["container"]}>
+      <div className={styles["container-search-button"]}>
+        <div className={styles["search-bar"]}>
+          <input type="text" placeholder="Filter by Status" />
+          <span className={styles["search-icon"]}>&#128269;</span>
+        </div>
+      </div>
+      <Table
+        columns={[
+          { key: "_id", title: "No" },
+          { key: "name", title: "Name" },
+          { key: "positionID", title: "ID" }, //placeholder
+          { key: "lastDate", title: "Last Date" },
+          { key: "aor", title: "AOR" },
+          { key: "checklist", title: "Action" }
+        ]}
+        data={checklistItems}
+        height="400px"
+      />
+    </div>
+  );
+}
