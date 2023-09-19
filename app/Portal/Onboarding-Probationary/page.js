36a3face42480c17eef912ed6db037632cd67bc7
DesmondDui0526 <desmonddui0526@gmail.com>
2023-09-19 21:38:00 +0800

    finish probationary page

--- app/Portal/Onboarding-Probationary/page.js
+++ app/Portal/Onboarding-Probationary/page.js
@@ -1,95 +1,47 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
 
-export default function Home() {
+import styles from './on.module.css';
+import Table from "../Table";
+import Link from "next/link";
+import { FaSearch } from "react-icons/fa";
+
+const checklistItems = [
+  {
+    _id: 1,
+    name: "John Doe",
+    positionID: "12345",
+    incharge: "Manager",
+    vendor: "Training Co.",
+    email: "john@example.com",
+  },
+];
+
+export default function Probationary() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className={styles["container"]}>
+        <div className={styles["container-search-button"]}>
+          <div className={styles["search-bar"]}>
+            <input type="text" placeholder="Filter by Status" />
+            <span className={styles["search-icon"]}><FaSearch/></span>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <Table
+          columns={[
+            { key: "_id", title: "No" },
+            { key: "name", title: "Name" },
+            { key: "positionID", title: "ID" },
+            { key: "startdate", title: "Start Date" },
+            { key: "enddate", title: "End Date" },
+            { key: "email", title: "Email Status", }
+          ]}
+          data={checklistItems}
+          height="400px"
+          />
+          <button className={styles["custom-button"]}>
+            <Link href="./NewEmployee">
+              <span className={styles["plus-icon"]}>&#43;</span>Add New Employee
+              </Link>
+              </button>
+        </div>
+    )
 }
