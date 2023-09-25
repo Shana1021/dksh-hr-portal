c0fa3c5a032239416e1b1c9a0138a6421c912e3d
Shana1021 <nakshatramaalah@gmail.com>
2023-09-25 23:34:34 +0800

    connecting with sidebar

--- app/Portal/SidebarData.js
+++ app/Portal/SidebarData.js
@@ -59,7 +59,7 @@ const sidebarData = [
       },
       {
         title: "Resignation Requests",
-        path: "#",
+        path: "/Portal/Offboarding-ResignationRequests",
       },
       {
         title: "Checklist",
