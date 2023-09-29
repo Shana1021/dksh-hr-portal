b1c293ea23a1ae4e2d796cc6c00bfdc25dfb3a71
DesmondDui0526 <desmonddui0526@gmail.com>
2023-09-29 18:11:32 +0800

    create datapicker calendar design and css
    install the npm datapicker
    
    create input field for days in acknowledged

--- app/EmployeeAcknowledgement/page.js
+++ app/EmployeeAcknowledgement/page.js
@@ -1,33 +1,29 @@
 "use client";
 
 import React, { useState } from "react";
-import Calendar from 'react-calendar'; 
-import { FaDownload, FaUpload} from 'react-icons/fa'; 
-import styles from './ack.module.css';
+import DatePicker from "react-datepicker"; 
+import { FaDownload, FaUpload } from "react-icons/fa";
+import styles from "./ack.module.css";
+import "react-datepicker/dist/react-datepicker.css"; 
 
 export default function AcknowledgedEmployees() {
   const [date, setDate] = useState(new Date());
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [numberOfDays, setNumberOfDays] = useState(0);
 
   const onChange = (newDate) => {
     setDate(newDate);
-  }
-  
-  const [inputValue, setInputValue] = useState('');
-
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
   };
 
   const handleDownloadClick = () => {
-    const fileUrl = 'https://example.com/path/to/your/file.pdf';
-
-    const link = document.createElement('a');
+    // Just testing purpose only
+    const fileUrl = "https://example.com/path/to/your/file.pdf";
+    // Just testing purpose only
+    const link = document.createElement("a");
     link.href = fileUrl;
-    link.download = 'filename.pdf'; 
+    link.download = "filename.pdf";
     link.click();
   };
-
-  const [selectedFile, setSelectedFile] = useState(null);
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -36,39 +32,57 @@ export default function AcknowledgedEmployees() {
 
   const handleUpload = () => {
     if (selectedFile) {
-     
       const formData = new FormData();
-      formData.append('file', selectedFile);
+      formData.append("file", selectedFile);
     } else {
-    
-      alert('Please select a file to upload.');
+      alert("Please select a file to upload.");
     }
   };
 
+  const handleNumberOfDaysChange = (event) => {
+    const value = event.target.value;
+    const parsedValue = parseInt(value, 10);
+
+    if (!isNaN(parsedValue)) {
+      setNumberOfDays(parsedValue);
+    }
+  };
+  
+
   return (
-    <div className={styles['page']}>
-      <div className={styles['acknowledged']}>
+    <div className={styles["page"]}>
+      <div className={styles["acknowledged"]}>
         <h1>Employee Acknowledgment</h1>
-        <input
-          type="text"
-          value={inputValue}
-          onChange={handleInputChange}
-          placeholder="Type words here..."
-        />
-        <p> Message: {inputValue}</p>
+        <p>Hi, </p>
+        <p>
+          After careful consideration, we have officially accepted your resignation and issued the
+          Acceptance of Resignation Letter (AOR). Do take note of the Annual Leave Balance,
+          <input
+            type="text"
+            onChange={handleNumberOfDaysChange}
+            className={styles["input-field"]}
+          />
+          {numberOfDays == 1 ? "day" : "days"}
+          <br />
+          <p>
+            {" "}
+            Please state your last day of work and upload back the signed AOR for our reference,
+            Thank you.{" "}
+          </p>
+        </p>
       </div>
 
-      <div className={styles['center-container']}>
-        <div className={styles['calendar-container']}>
+      <div className={styles["center-container"]}>
+        <div className={styles["calendar-container"]}>
           <h2>End Date</h2>
-          <Calendar
+          <DatePicker
+            selected={date}
             onChange={onChange}
-            value={date}
-            className={styles['calendar']}
+            className={styles["calendar"]} // You can apply your CSS styles here
           />
         </div>
 
-        <div className={styles['resignation-letter']}>
+        <div className={styles["resignation-letter"]}>
           <h2>Acceptance of Resignation Letter</h2>
           <h3>Download the AOR file here</h3>
           <div>
@@ -78,22 +92,18 @@ export default function AcknowledgedEmployees() {
           </div>
         </div>
 
-        <div className={styles['aor-letter']}>
+        <div className={styles["aor-letter"]}>
           <h2>Return of AOR</h2>
           <h3>Upload your signed file here</h3>
           <div>
-            <input 
-              type="file"
-              accept=".pdf" 
-              onChange={handleFileChange}
-            />
+            <input type="file" accept=".pdf" onChange={handleFileChange} />
             <button onClick={handleUpload}>
-              Upload <FaUpload/>
+              Upload <FaUpload />
             </button>
           </div>
         </div>
       </div>
-      <div className={styles["button"]}>
+      <div className={styles["button-container"]}>
         <button className="module-button">SAVE & SUBMIT</button>
       </div>
     </div>
