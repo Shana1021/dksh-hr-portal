edddc4d3633cc83b85ac68d7d6d01188757ecd43
rasul <rasulp99@.com>
2023-10-13 19:39:52 +0800

    global button and header title

--- app/Portal/Settings/SettingsPage.js
+++ app/Portal/Settings/SettingsPage.js
@@ -11,9 +11,8 @@ export default function SettingsPage({
   initialAdminTeamEmailAddress,
   initialHRTeamEmailAddress,
 }) {
- // const router = useRouter();
+  // const router = useRouter();
   const [activeTab, setActiveTab] = useState("Profile"); // State to manage the active tab
-
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -26,7 +25,7 @@ export default function SettingsPage({
       alert(res.statusText);
       return;
     }
-    
+
     router.refresh();
   }
 
@@ -37,71 +36,73 @@ export default function SettingsPage({
 
       {/* Content for each tab */}
       {activeTab === "Profile" && (
-         <div className={styles.tabContent}>
+        <div className={styles.tabContent}>
           <div className={styles.tabContentBox}>
-          {/*Profile*/}
-          <Profile />
-         </div>
-         </div>
-    
+            {/*Profile*/}
+            <Profile />
+          </div>
+        </div>
       )}
 
       {/* Email tab */}
       {activeTab === "Email" && (
         <div className={styles.tabContent}>
-           <div className={styles.tabContentBox}>
-          <form className={styles["recipient-email-settings"]} onSubmit={handleSubmit}>
-            <table>
-              <tbody>
-                <tr>
-                  <td>IT Team Email Address:</td>
-                  <td>
-                    <input
-                      type="email"
-                      name="itTeamEmailAddress"
-                      placeholder="it@dksh.com"
-                      defaultValue={initialITTeamEmailAddress}
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <td>Admin Team Email Address:</td>
-                  <td>
-                    <input
-                      type="email"
-                      name="adminTeamEmailAddress"
-                      placeholder="admin@dksh.com"
-                      defaultValue={initialAdminTeamEmailAddress}
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <td>HR Team Email Address:</td>
-                  <td>
-                    <input
-                      type="email"
-                      name="hrTeamEmailAddress"
-                      placeholder="hr@dksh.com"
-                      defaultValue={initialHRTeamEmailAddress}
-                    />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-            <button type="submit" className="module-button">
-              Save
-            </button>
-          </form>
-        </div>
+          <div className={styles.tabContentBox}>
+            <form
+              className={styles["recipient-email-settings"]}
+              onSubmit={handleSubmit}
+            >
+              <table>
+                <tbody>
+                  <tr>
+                    <td>IT Team Email Address:</td>
+                    <td>
+                      <input
+                        type="email"
+                        name="itTeamEmailAddress"
+                        placeholder="it@dksh.com"
+                        defaultValue={initialITTeamEmailAddress}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Admin Team Email Address:</td>
+                    <td>
+                      <input
+                        type="email"
+                        name="adminTeamEmailAddress"
+                        placeholder="admin@dksh.com"
+                        defaultValue={initialAdminTeamEmailAddress}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>HR Team Email Address:</td>
+                    <td>
+                      <input
+                        type="email"
+                        name="hrTeamEmailAddress"
+                        placeholder="hr@dksh.com"
+                        defaultValue={initialHRTeamEmailAddress}
+                      />
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <button type="submit" className="module-button">
+                Save
+              </button>
+            </form>
+          </div>
         </div>
       )}
 
       {activeTab === "Account" && (
         <div className={styles.tabContent}>
-           <div className={styles.tabContentBox}>
-           <h5 className="test">Test</h5>
-          {/* Add content for the Account tab here */}
-        </div>
+          <div className={styles.tabContentBox}>
+            <h5 className="test">Test</h5>
+            {/* Add content for the Account tab here */}
+          </div>
         </div>
       )}
     </>
