27d63ba766f382b22572fd59461ace14b7a40156
rasul <rasulp99@.com>
2023-10-16 23:29:25 +0800

    EmployeeAcknowledgement changes

--- app/EmployeeAcknowledgement/[id]/EmployeeAcknowledgementPage.js
+++ app/EmployeeAcknowledgement/[id]/EmployeeAcknowledgementPage.js
@@ -14,10 +14,15 @@ export default function EmployeeAcknowledgementPage({ acceptedResignation }) {
   async function handleSubmit(e) {
     e.preventDefault();
 
-    const res = await fetch(`/api/acknowledge-resignation/${encodeURIComponent(acceptedResignation._id)}`, {
-      method: "POST",
-      body: new FormData(e.target)
-    });
+    const res = await fetch(
+      `/api/acknowledge-resignation/${encodeURIComponent(
+        acceptedResignation._id
+      )}`,
+      {
+        method: "POST",
+        body: new FormData(e.target),
+      }
+    );
     if (!res.ok) {
       alert(res.statusText);
       return;
@@ -33,58 +38,103 @@ export default function EmployeeAcknowledgementPage({ acceptedResignation }) {
           <BsFillCheckCircleFill size="70" color="green" />
           <h1>Thank You</h1>
           <p>Your response has been recorded.</p>
-          
-          <div className={styles["button-container"]} style={{ marginTop: 20 , marginRight: 35}}>
+
+          <div
+            className={styles["button-container"]}
+            style={{ marginTop: 20, marginRight: 35 }}
+          >
             <button onClick={() => setSubmitted(false)}>Back to Form?</button>
           </div>
         </div>
       </div>
-    )
+    );
   }
 
   return (
     <form className={styles["page"]} onSubmit={handleSubmit}>
+      <div className={styles["logo"]}>
+        <Image
+          src="/dksh_logo.png"
+          alt="DKSH Logo"
+          fill
+          style={{ objectFit: "contain" }}
+        />
+      </div>
       <div className={styles["acknowledged"]}>
-        <div className={styles["logo"]}>
-          <Image
-            src="/dksh_logo.png"
-            alt="DKSH Logo"
-            fill
-            style={{ objectFit: "contain" }}
-          />
-        </div>
-        <h1>Employee Acknowledgement</h1>
-        <p>Hi <b>{acceptedResignation.profile.firstName} {acceptedResignation.profile.lastName}</b></p>
+        <h1>
+          <span>Employee Acknowledgement</span>
+        </h1>
         <p>
-          After careful consideration, we have officially accepted your resignation and issued the
-          Acceptance of Resignation Letter (AOR).
-          Do take note of the Annual Leave Balance: <b>{acceptedResignation.annualLeaveBalance} day(s)</b>.
+          Hi{" "}
+          <b>
+            <u>
+              {acceptedResignation.profile.firstName}{" "}
+              {acceptedResignation.profile.lastName}
+            </u>
+          </b>
         </p>
-        <p>Please state your last day of work and re-upload the signed AOR for our reference, Thank you.</p>
+        <p>
+          After careful consideration, we have officially accepted your
+          resignation and issued the Acceptance of Resignation Letter (AOR). Do
+          take note of the Annual Leave Balance:{" "}
+          <b>
+            <u>{acceptedResignation.annualLeaveBalance} day(s)</u>
+          </b>
+          .
+        </p>
+        <p>
+          Please state your last day of work and re-upload the signed AOR for
+          our reference, Thank you.
+        </p>
       </div>
 
       <div className={styles["row"]}>
         <div className={styles["calendar"]}>
-          <h1>End Date</h1>
+          <h1>
+            <span>End Date</span>
+          </h1>
           <div className={styles["react-calendar-medium"]}>
             <input type="hidden" name="endDate" value={endDate.toISOString()} />
-            <Calendar value={endDate} onChange={date => setEndDate(date)} />
-            <p>Selected date is: <b>{endDate.toDateString()}</b></p>
+            <Calendar value={endDate} onChange={(date) => setEndDate(date)} />
+            <p>
+              Selected date is:{" "}
+              <b>
+                <u>{endDate.toDateString()}</u>
+              </b>
+            </p>
           </div>
         </div>
 
         <div className={styles["column"]}>
           <div className={styles["resignation-letter"]}>
-            <h1>Acceptance of Resignation Letter</h1>
+            <h1>
+              <span>Acceptance of Resignation Letter</span>
+            </h1>
             <h2>Download the AOR file here</h2>
-            <a href={`/api/offboarding/${encodeURIComponent(acceptedResignation._id)}/aor`} download>
-              <button type="button">Download AOR <FaDownload /></button>
+            <a
+              href={`/api/offboarding/${encodeURIComponent(
+                acceptedResignation._id
+              )}/aor`}
+              download
+            >
+              <button type="button">
+                Download AOR <FaDownload />
+              </button>
             </a>
           </div>
           <div className={styles["aor-letter"]}>
-            <h1>Return of AOR</h1>
+            <h1>
+              <span>Return of AOR</span>
+            </h1>
             <h2>Upload your signed file here</h2>
-            <input className={styles["upload"]} name="aor" type="file" required />
+            <button type="button">
+              <input
+                className={styles["upload"]}
+                name="aor"
+                type="file"
+                required
+              />
+            </button>
           </div>
 
           <div className={styles["button-container"]}>
