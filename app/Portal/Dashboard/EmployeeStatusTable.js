56134ca22f9fb1c125dd6bd8255ebafc7775e3aa
rasul <rasulp99@.com>
2023-10-18 23:57:12 +0800

    dashboardDraft

--- app/Portal/Dashboard/EmployeeStatusTable.js
+++ app/Portal/Dashboard/EmployeeStatusTable.js
@@ -32,23 +32,7 @@ export default function EmployeeStatusTable() {
               <td>TR</td>
               <td>Pending</td>
             </tr>
-            <tr>
-              <td>
-                <span className={styles["image-container"]}>
-                  <Image
-                    src="/dksh_logo.png"
-                    alt="DKSH Logo"
-                    width={50}
-                    height={25}
-                  />
-                </span>
-                Jared
-              </td>
-              <td>Accounting</td>
-              <td>35</td>
-              <td>BC</td>
-              <td>Complete</td>
-            </tr>
+
             <tr>
               <td>
                 <span className={styles["image-container"]}>
@@ -65,23 +49,6 @@ export default function EmployeeStatusTable() {
               <td>20</td>
               <td>AOR</td>
               <td>Incomplete</td>
-            </tr>
-            <tr>
-              <td>
-                <span className={styles["image-container"]}>
-                  <Image
-                    src="/dksh_logo.png"
-                    alt="DKSH Logo"
-                    width={50}
-                    height={25}
-                  />
-                </span>
-                Desmond
-              </td>
-              <td>Sales</td>
-              <td>40</td>
-              <td>BC</td>
-              <td>Complete</td>
             </tr>
           </tbody>
         </table>
