82c70956f37e40938b1370c558657ad0924c2e69
rasul <rasulp99@.com>
2023-10-26 18:08:03 +0800

    Acknowledge Resignation

--- app/Portal/Offboarding-ResignationRequests/OffboardingResignationRequestPage.js
+++ app/Portal/Offboarding-ResignationRequests/OffboardingResignationRequestPage.js
@@ -5,15 +5,21 @@ import styles from "./offboarding-resignationrequests.module.css";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
-function AcknowledgeResignationPopup({ name, department, reason, onClose, onConfirm }) {
+function AcknowledgeResignationPopup({
+  name,
+  department,
+  reason,
+  onClose,
+  onConfirm,
+}) {
   const [accepted, setAccepted] = useState(true);
 
   return (
     <div className={styles["modal-overlay"]} onClick={onClose}>
       <form
         className={styles["modal"]}
-        onClick={e => e.stopPropagation()}
-        onSubmit={e => {
+        onClick={(e) => e.stopPropagation()}
+        onSubmit={(e) => {
           e.preventDefault();
 
           onConfirm(new FormData(e.target));
@@ -26,11 +32,17 @@ function AcknowledgeResignationPopup({ name, department, reason, onClose, onConf
 
         <div className={styles["scroll-container"]}>
           <div className={styles["modal-content"]}>
-            <div><b>Employee Name:</b> {name}</div>
-            <div><b>Department:</b> {department}</div>
-            <div><b>Reason:</b> {reason}</div>
+            <div className={styles["header"]}>
+              <b>Employee Name:</b> {name}
+            </div>
+            <div className={styles["header"]}>
+              <b>Department:</b> {department}
+            </div>
+            <div className={styles["header"]} id="last">
+              <b>Reason:</b> {reason}
+            </div>
           </div>
-          
+
           <div className={styles["modal-content"]}>
             <h4 className={styles["h4"]}>Response</h4>
             <div className={styles["radio-button-group"]}>
@@ -67,11 +79,12 @@ function AcknowledgeResignationPopup({ name, department, reason, onClose, onConf
               placeholder="e.g. 5"
               min="0"
               required={accepted}
-            /> days
+            />{" "}
+            days
           </div>
 
           <div className={styles["modal-content"]}>
-            <h4 className={styles["h4"]}>Manager Email</h4>
+            <h4 className={styles["h4"]}>Manager Email Address</h4>
             <input
               className={styles["input-field"]}
               type="email"
@@ -83,15 +96,14 @@ function AcknowledgeResignationPopup({ name, department, reason, onClose, onConf
 
           <div className={styles["modal-content"]}>
             <h4 className={styles["h4"]}>Upload Acceptance Letter</h4>
-            <input
-              type="file"
-              name="acceptanceLetter"
-              required={accepted}
-            />
+            <input type="file" name="acceptanceLetter" required={accepted} />
           </div>
         </div>
 
-        <button className={`module-button ${styles["confirm-btn"]}`} type="submit">
+        <button
+          className={`module-button ${styles["confirm-btn"]}`}
+          type="submit"
+        >
           Confirm
         </button>
       </form>
@@ -99,7 +111,9 @@ function AcknowledgeResignationPopup({ name, department, reason, onClose, onConf
   );
 }
 
-export default function OffboardingResignationRequestsPage({ resignationRequests }) {
+export default function OffboardingResignationRequestsPage({
+  resignationRequests,
+}) {
   const router = useRouter();
   const [selectedIndex, setSelectedIndex] = useState(null);
 
@@ -107,18 +121,29 @@ export default function OffboardingResignationRequestsPage({ resignationRequests
     <>
       <div className={styles["container"]}>
         {resignationRequests.map((resignationRequest, index) => (
-          <div key={resignationRequest._id} className={styles["card"]} onClick={() => setSelectedIndex(index)}>
+          <div
+            key={resignationRequest._id}
+            className={styles["card"]}
+            onClick={() => setSelectedIndex(index)}
+          >
             <div className={styles["card-img"]}>
               <Image
-                src={`/api/onboarding/${encodeURIComponent(resignationRequest._id)}/profileImage`}
+                src={`/api/onboarding/${encodeURIComponent(
+                  resignationRequest._id
+                )}/profileImage`}
                 alt="Profile Image"
                 fill
                 style={{ objectFit: "cover" }}
               />
             </div>
             <div className={styles["card-body"]}>
-              <h4>{resignationRequest.profile.firstName} {resignationRequest.profile.lastName}</h4>
-              <p className={styles["department"]}>{resignationRequest.profile.department}</p> 
+              <h4>
+                {resignationRequest.profile.firstName}{" "}
+                {resignationRequest.profile.lastName}
+              </h4>
+              <p className={styles["department"]}>
+                {resignationRequest.profile.department}
+              </p>
             </div>
           </div>
         ))}
@@ -129,11 +154,16 @@ export default function OffboardingResignationRequestsPage({ resignationRequests
           department={resignationRequests[selectedIndex].profile.department}
           reason={resignationRequests[selectedIndex].reason}
           onClose={() => setSelectedIndex(null)}
-          onConfirm={async formData => {
-            const res = await fetch(`/api/offboarding/${encodeURIComponent(resignationRequests[selectedIndex]._id)}`, {
-              method: "POST",
-              body: formData
-            });
+          onConfirm={async (formData) => {
+            const res = await fetch(
+              `/api/offboarding/${encodeURIComponent(
+                resignationRequests[selectedIndex]._id
+              )}`,
+              {
+                method: "POST",
+                body: formData,
+              }
+            );
             if (!res.ok) {
               alert(res.statusText);
               return;
